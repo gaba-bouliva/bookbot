d2c30576bafee28bcfc5d82c05558732754b9a38
bgaba <>
2024-10-22 07:11:00 +0200

    add func to count words in book text

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+def word_count(text):
+  return len(text.split())
+
 try:
   file = open("books/frankenstein.txt", "r")
-  file_content = file.readlines()
-  print(file_content)
+  file_content = file.read()
+  print(word_count(file_content))
 except FileNotFoundError :
   print("invalid file name or filepath provided")
 except Exception as e:
